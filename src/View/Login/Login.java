0c7ef9b4065a2480eb26d6ea5df17120a3d3f594
teresafortes19 <62069074+teresafortes019@users.noreply.github.com>
2021-12-27 23:01:49 +0000

    Interface Login criada

---
+++ src/View/Login/Login.java
@@ -0,0 +1,10 @@
+package View.Login;
+
+import javax.swing.*;
+
+public class Login {
+    private JTextField textField1;
+    private JPanel panel1;
+    private JPasswordField passwordField1;
+    private JButton button1;
+}
