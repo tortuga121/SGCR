b18696ae7991eaa0bda3d879163f231279ad0360
Gonçalo Pinto <blanc@blanc-mac.lan>
2021-12-30 21:06:16 +0000

    controller budget refuse

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -12,11 +12,8 @@ import View.Login.VLogin;
 import View.Manager.VManager;
 import View.Receptionist.*;
 import View.Technician.VTechnician;
-
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -48,7 +45,7 @@ public class Controller implements IController{
                 }
                 else view.showPopUpMsg("Credenciais inválidas.");
             } catch (NumberFormatException | WorkerDoesNotExist exception) {
-                view.showPopUpMsg("Credenciais inválidas.");
+                view.showPopUpMsg(exception.getMessage());
             }
         });
     }
@@ -90,20 +87,34 @@ public class Controller implements IController{
                 List<String> l = new ArrayList<>();
                 for (Integer i : regCodes) {
                     String name = "0";
-                    String s = i + " - Código de registo; " + sgcr.getDeviceName(i) + " - Nome equipamento.";
+                    String s = i + " - Código de registo; " + sgcr.getDevice(i).getName() + " - Nome equipamento.";
                     l.add(s);
                 }
                 VDeviceList vdl = new VDeviceList(nif, l);
+                vdl.setVisible(true);
                 vdl.getSelectButton().addActionListener(e1 -> {
                     String selectedValue = vdl.getDeviceList().getSelectedValue();
                     String regCodeStr = selectedValue.split(" -")[0];
                     int regCode = Integer.parseInt(regCodeStr);
-                    //VDevice vDevice = new VDevice();
+                    try {
+                        Device device = (Device) sgcr.getDevice(regCode);
+                        VDevice vDevice = new VDevice(device);
+                        vDevice.setVisible(true);
+                        view.showPopUpMsg("Para recusar o plano de reparação clique em confirmar.\nCaso contrário feche a janela.");
+                        vDevice.getDoneButton().addActionListener(e2 -> {
+                            try {
+                                LocalDate deadline = sgcr.refuseBudget(regCode);vDevice.dispose();
+                                vdl.dispose();
+                                view.showPopUpMsg("Reparação cancelada.\nTem até " + deadline.toString() + " para levantar o seu equipamento.");
+                            } catch (DeviceNotFoundException | WorkerDoesNotExist ex) {
+                                view.showPopUpMsg(ex.getMessage());
+                            }
+                        });
+                    } catch (DeviceNotFoundException ignored) {}
                 });
             } catch (DeviceNotFoundException ex) {
-                view.showPopUpMsg("NIF Inválido");
+                view.showPopUpMsg(ex.getMessage());
             }
-            //view.showPopUpMsg("Reparação cancelada.\nTem até " + deadline.toString() + " para levantar o seu equipamento.");
         });
     }
 
@@ -122,7 +133,7 @@ public class Controller implements IController{
                 vbr.dispose();
                 view.showPopUpMsg("Pedido de orçamento adicionado.");
             } catch (NumberFormatException | WorkerDoesNotExist | InvalidRegistrationCodeException ex) {
-                view.showPopUpMsg("Formulário inválido.");
+                view.showPopUpMsg(ex.getMessage());
             }
         });
     }
