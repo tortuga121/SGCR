e235338a05500b115078cd31facbe66f90ebcf96
tortuga <daniel2001faria@gmail.com>
2021-12-31 05:57:25 +0000

    FETSEJA2

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main(String[] args) throws InvalidRegistrationCodeException, WorkerDoesNotExist, DeviceNotFoundException, NoMoreStepsException, NoRepairException, OutOfbudgetException {
         SGCR sgcr = new SGCR();
 
-        ArrayList<IDevice> arr = new ArrayList<>();
+       /* ArrayList<IDevice> arr = new ArrayList<>();
         arr.add( new Device(0,"000000000","este é o 1", "pc"));
         arr.add( new Device(1,"000000000","este é o 2", "tele"));
         arr.add( new Device(2,"000000000", "este é o 3","maquina"));
@@ -22,7 +22,7 @@ public class Main {
         arr.add( new Device(5,"666666666", "este é o 6","smartwatch"));
         arr.add( new Device(6,"888888888", "este é o 7","televisoa"));
         arr.add( new Device(7,"888888888", "este é o 8","fones"));
-        arr.add( new Device(8,"999999999", "este é o 9","coluna"));
+        arr.add( new Device(8,"999999999", "este é o 9","coluna"));*/
 
 
         sgcr.addWorker("shaggy",122,Receptionist.class,"122");
@@ -31,7 +31,7 @@ public class Main {
         sgcr.addWorker("tortuga",121,Technician.class,"121");
         sgcr.addWorker("blanc",157,Manager.class,"157");
 
-        for(IDevice d : arr) sgcr.addBudgetRequest(d,129);
+      /*  for(IDevice d : arr) sgcr.addBudgetRequest(d,129);
         ArrayList<Step> s = new ArrayList<>();
         s.add(new Step("este é o step 1"));
         s.add(new Step("este é o step2"));
@@ -42,7 +42,7 @@ public class Main {
         ss.add(new Stage("este é o stage",s));
         ss.add(new Stage("este é o stage2",s2));
         sgcr.sugestRepairPlan(new RepairPlan(0,"arranja", ss,200, LocalDate.now().plusDays(2)));
-        sgcr.acceptBudget(0);
+        sgcr.acceptBudget(0);*/
         IController c = new Controller(sgcr);
         c.exec();
     }
