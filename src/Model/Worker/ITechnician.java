cd3ea2f275fc0a506e64f55c7d7db6fd02881b82
teresafortes19 <62069074+teresafortes019@users.noreply.github.com>
2021-12-31 03:01:58 +0000

    documentação de várias interfaces

--- src/Model/Worker/ITechnician.java
+++ src/Model/Worker/ITechnician.java
@@ -1,6 +1,11 @@
 package Model.Worker;
 
 public interface ITechnician  extends IWorker{
+    /**
+     * Método que adiciona uma reparação ao Set de reparações de um técnico
+     * @param regCode código do equipamento
+     * @param stage etapa da reparação que o técnico vai realizar nesse equipamento
+     */
     void addParticipation(int regCode, int stage);
     int getTotalParticipations();
     void setAvailable(boolean available);
